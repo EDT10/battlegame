46abab61c6a0ea5512204f8432308ca3533a3364
Emmanuel Wright <emmanuelnwright@gmail.com>
2022-07-05 17:43:17 -0400

    added code that display the result of the battle

--- battlegame.py
+++ battlegame.py
@@ -1,3 +1,5 @@
+print("********** WELCOME TO MY BATTLEGAME. GOOD LUCK! **********")
+
 # TASK 1
 
 wizard = "Wizard"
@@ -15,35 +17,34 @@ human_damage = 20
 dragon_damage = 50
 
 
-# TASK 2
+# #TASK 2
 # print("1-Wizard")
 # print("2-Elf")
 # print("3-Human")
 # print("4-Dragon")
 
-# character = int(input("Choose your character:"))
+# character = input("Choose your character:").lower()
 
 # TASK 3
 
 while True:
-    print("1-Wizard")
-    print("2-Elf")
-    print("3-Human")
-    print("4-Dragon")
+    print("1.) Wizard")
+    print("2.) Elf")
+    print("3.) Human")
 
-    character = int(input("Choose your character:"))
+    character = input("Choose your character:").lower()
 
-    if character == 1:
+    if character == "1" or character == "Wizard":
         character = wizard
         my_hp = wizard_hp
         my_damage = wizard_damage
         break
-    elif character == 2:
+    elif character == "2" or character == "Elf":
         character = elf
         my_hp = elf_hp
         my_damage = elf_damage
         break
-    elif character == 3:
+    elif character == "3" or character == "Human":
         character = human
         my_hp = human_hp
         my_damage = human_damage
@@ -52,4 +53,25 @@ while True:
         print("Unknown character")
         break
 
+print("----")
 print("You've chosen the character:", character,"with damage:", my_damage, "and horse power of:", my_hp)
+print("----")
+
+while True:
+    dragon_hp = dragon_hp - my_damage
+    print("The", character, "damaged the Dragon")
+    print("The Dragon hits point are now", dragon_hp)
+    print("----")
+    if dragon_hp <= 0:
+        print("The Dragon has lost the battle")
+        break
+
+    my_hp = my_hp - dragon_damage
+    print("Your", character, "HP is now", my_hp, "because the Dragon hit you")
+
+    if my_hp <= 0:
+        print("The", character, "lost the battle")
+        break
+    print("----")    
+             
+  
